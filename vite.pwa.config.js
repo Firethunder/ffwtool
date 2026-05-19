aa79a25d699aac33c9d0d148e2093d64129dd274
Robert Edelmann <robert-edelmann@web.de>
2026-05-19 22:41:47 +0200

    (fix) ffwpwa invalid assets

--- vite.pwa.config.js
+++ vite.pwa.config.js
@@ -6,7 +6,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/ffwpwa/',
-  publicDir: resolve(__dirname, 'public'),
+  publicDir: false,
   plugins: [
     vue(),
     VitePWA({
